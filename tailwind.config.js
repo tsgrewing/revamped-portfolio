96ce8847b42637ab59496a4f0096d7c936516529
Tony G <63308979+tsgrewing@users.noreply.github.com>
2023-03-10 00:50:20 -0600

    updating safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: [
+      'text-red-200',
+      'text-red-300',
+      'text-red-400',
+      'text-red-500',
+      'text-red-600',
+      'text-red-700'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
